0bcec06fb50de8941d3339d41a2adbc04fa0d97f
Aakash <aakashanand1995@gmail.com>
2024-03-07 19:26:38 +0530

    formData fixed

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 
 const FormHandle = () => {
-  const { formData, setFormData } = useState([]);
+  const [formData, setFormData] = useState("");
 
-  function handleSubmit() {}
+  function handleSubmit(event: React.ChangeEvent<HTMLInputElement>) {
+    setFormData(event?.target.value);
+  }
+
+  console.log(formData);
 
   return (
     <>
       <div className="flex flex-col pt-10 h-auto justify-center items-center">
         <h1 className="dark:text-white text-2xl">Company Information</h1>
+        <h3 className="dark:text-white text-2xl">
+          Entered Company Name : {formData}
+        </h3>
         <form className="flex flex-col pt-2" action="/formsubmit" method="post">
           <input
             className="p-2 mb-2"
             type="text"
             name="company"
             id="company"
+            value={formData}
             onChange={handleSubmit}
             placeholder="Enter Company Name/Symbol"
           />
